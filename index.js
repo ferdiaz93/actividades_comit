d2a8d5c30efc418df9c3e7e8fbd17f3e4759914d
Fernando Diaz <fernandodiaz1209@gmail.com>
2023-07-18 18:46:22 -0300

    ejercicios comentados

--- index.js
+++ index.js
@@ -59,3 +59,122 @@
 //         console.log(num1 / num2);
 //     }
 // };
+
+
+// function encontrarPalabraMasLarga(frase){
+//     let arrayDePalabras = frase.split(" ");
+//     let palabraMasLarga = "";
+//     for(let palabra of arrayDePalabras){
+//         if(palabra.length > palabraMasLarga.length){
+//             palabraMasLarga = palabra;
+//         }
+//     }
+//     console.log(palabraMasLarga);
+// }
+
+// let persona1 = {
+//     nombre: "Pedro",
+//     edad: 15
+// }
+
+// let persona2 = {
+//     nombre: "Ana",
+//     edad: 28,
+//     esMayorDeEdad: function(){
+//         if(persona.edad >= 18){
+//             console.log(`${persona.nombre} es mayor de edad.`)
+//         } else {
+//             console.log(`${persona.nombre} no es mayor de edad.`)
+//         }
+//     }
+// }
+
+// function esMayorDeEdad(persona){
+//     if(persona.edad >= 18){
+//         console.log(`${persona.nombre} es mayor de edad.`)
+//     } else {
+//         console.log(`${persona.nombre} no es mayor de edad.`)
+//     }
+// }
+
+
+// esMayorDeEdad(persona1);
+// esMayorDeEdad(persona2);
+
+
+// let numeros = [1, 2, 3, 4];
+// let numerosDuplicados = numeros.filter((numero) => {
+//     return numero >= 3;
+// })
+// console.log(numerosDuplicados)
+
+
+// let persona1 = {
+//     nombre: "Juan",
+//     anioDeNacimiento: 1993
+// }
+
+// let persona2 = {
+//     nombre: "Pedro",
+//     anioDeNacimiento: 2000,
+//     saludar: function(){
+
+//     }
+// }
+
+// function agregarEdad(persona){
+//     persona.edad = 2023 - persona.anioDeNacimiento;
+// }
+// let persona1 = {
+//     nombre: "fer",
+//     edad: 25
+// }
+// let persona2 = {
+//     nombre: "fer",
+//     edad: 25
+// }
+// let personas = [persona1, persona2];
+
+// function filtrarMayoresDeEdad(personasArray){
+//     let filtrados = [];
+//     for(let persona of personasArray){
+//         if(persona.edad >= 18){
+//             filtrados.push(persona);
+//         }
+//     }
+//     // let filtrados = personasArray.filter(persona => returnpersona.edad >= 18);
+//     console.log(filtrados);
+// }
+// filtrarMayoresDeEdad(personas);
+
+
+// const elementoTitulo = document.getElementById("titulo");
+// const seccionNueva = document.getElementById("nuevaSeccion");
+// const botonAgregar = document.getElementById("boton");
+// const botonCambiarColor = document.getElementById("botonEstilos");
+// let darkMode = false;
+
+// // console.log(elementoTitulo);
+// elementoTitulo.innerText = "hola";
+
+// botonAgregar.addEventListener("click", function(){
+//     let subtitulo = document.createElement('h2');
+//     subtitulo.innerText = "hola soy un h2";
+//     seccionNueva.appendChild(subtitulo);
+// });
+
+// botonCambiarColor.addEventListener("mouseenter", function(){
+    
+//     // document.body.style.backgroundColor = darkMode ? "white" : "black";
+//     // darkMode = !darkMode;
+    
+//     if(darkMode){
+//         elementoTitulo.classList.add('oculto');
+//         darkMode = false;
+//         // document.body.style.backgroundColor = "white";
+//     } else {
+//         elementoTitulo.classList.remove('oculto');
+//         darkMode = true;
+//         // document.body.style.backgroundColor = "black";
+//     }
+// });
