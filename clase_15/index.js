de2dce876beb9a8c93b9e8486f7cbf72d49c69f1
Fernando Diaz <fernandodiaz1209@gmail.com>
2023-07-27 21:33:29 -0300

    ejercicio clase 15 agregado

---
+++ clase_15/index.js
@@ -0,0 +1,89 @@
+// function pedirDatos(){
+//     console.log("Promesa creada");
+//     return new Promise(function (resolve, reject){
+//         setTimeout(function(){
+//             let misDatos = {name: "fernando", edad: 29};
+//             if(misDatos.edad > 18){
+//                 resolve(misDatos)
+//             } else {
+//                 reject("No es mayor de edad");
+//             }
+//         }, 5000);
+//     })
+// }
+
+
+
+// miPromesa
+//     .then((respuesta) => {
+//         console.log("resuelta", respuesta);
+//     })
+//     .catch((respuesta) => {
+//         console.log("rechazada", respuesta);
+//     })
+//     .finally(() => {
+//         console.log("promesa terminada")
+//     })
+
+const ulElement = document.getElementById('lista');
+const ulElementAlbums = document.getElementById('lista-albums');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((usuarios) => {
+        console.log(usuarios);
+        usuarios.forEach(usuario => crearItemDeUsuario(usuario));
+    })
+
+function crearItemDeUsuario(usuario){
+    let liElement = document.createElement('li');
+    let buttonElement = document.createElement('button');
+    buttonElement.innerText = usuario.name;
+    
+    liElement.addEventListener('click', function(){
+        console.log(usuario.id)
+        mostrarAlbumsDelUsuario(usuario.id)
+    })
+    
+    liElement.appendChild(buttonElement);
+    ulElement.appendChild(liElement);
+
+}
+
+function mostrarAlbumsDelUsuario(idUser){
+    fetch('https://jsonplaceholder.typicode.com/albums?userId=' + idUser)
+    .then(response => response.json())
+    .then((albums) => {
+        console.log(albums);
+        ulElementAlbums.innerHTML = "";
+        albums.forEach(function(album){
+            // crear un li (document.createElement)
+            let li = document.createElement('li');
+            // agregar contenido (album.title) con el .innerText 
+            li.innerText = album.title;
+            // insertar el li dentro de un nuevo ul (capturar nuevo ul)
+            ulElementAlbums.appendChild(li);
+            // agregar el evento click al li, que al hacer click mostrar el id del album por consola
+            li.addEventListener('click', function() {
+                console.log(album.id);
+                mostrarFotosDelAlbum(album.id);
+            })
+        })
+    })
+}
+
+function mostrarFotosDelAlbum(idAlbum){
+    fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + idAlbum)
+        .then(response => response.json())
+        .then((photos) => {
+            console.log(photos)
+            //recorrer las fotos con un forEach y por cada uno hacer lo siguiente:
+                //crear un article
+                //crear un img
+                //crear un h4
+                //al h4 darle contenido (title)
+                //img agregar el atributo 'src' y el valor debe ser la url
+                // insertar los elementos dentro del article
+                //insertar el article dentro de una section (que debemos capturar de nuestro html)
+        })
+}
