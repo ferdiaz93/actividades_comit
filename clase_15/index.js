930e2a76f9749f50c3a09297778c30db0620bdf4
Fernando Diaz <fernandodiaz1209@gmail.com>
2023-08-01 21:39:15 -0300

    ejercicio clase 15 agregado

--- clase_15/index.js
+++ clase_15/index.js
@@ -27,6 +27,7 @@
 
 const ulElement = document.getElementById('lista');
 const ulElementAlbums = document.getElementById('lista-albums');
+const sectionGrilla = document.getElementById('grilla-fotos');
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
@@ -76,14 +77,25 @@ function mostrarFotosDelAlbum(idAlbum){
     fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + idAlbum)
         .then(response => response.json())
         .then((photos) => {
-            console.log(photos)
+            console.log(photos);
             //recorrer las fotos con un forEach y por cada uno hacer lo siguiente:
+            photos.forEach(function(photo){
                 //crear un article
+                const article = document.createElement('article');
                 //crear un img
+                const img = document.createElement('img');
                 //crear un h4
+                const span = document.createElement('span');
+                
                 //al h4 darle contenido (title)
+                span.innerText = photo.title;
                 //img agregar el atributo 'src' y el valor debe ser la url
+                img.setAttribute('src', photo.url);
                 // insertar los elementos dentro del article
+                article.appendChild(img);
+                article.appendChild(span);
                 //insertar el article dentro de una section (que debemos capturar de nuestro html)
+                sectionGrilla.appendChild(article);
+            });
         })
 }
