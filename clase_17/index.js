cb60d43d39c3aac50f3189eff6e70f2bd19f942a
Fernando Diaz <fernandodiaz1209@gmail.com>
2023-08-03 21:45:44 -0300

    ejercicio clase 17

---
+++ clase_17/index.js
@@ -0,0 +1,46 @@
+// -Crear un formulario con los inputs “titulo”, “body” y “userId” con un boton enviar. -> HTML
+// -Darle al formulario el evento submit (addEventListener)
+const fomulario = document.getElementById('formulario');
+const mensajeEl = document.getElementById('mensaje');
+
+fomulario.addEventListener('submit', function(event){
+    event.preventDefault();
+    // capturar los inputs
+    let title = fomulario['title'].value;
+    let body = fomulario['body'].value;
+    let userId = fomulario['userId'].value;
+    console.log({title, body, userId});
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: title,
+            body: body,
+            userId: userId,
+        })
+    })
+    .then(response => {
+        console.log(response);
+        mostrarMensaje('Salio todo bien', true)
+    })
+    .catch(error => {
+        console.log(error)
+        mostrarMensaje('Salio todo mal', false)
+    })
+})
+
+function mostrarMensaje(texto, todoOk){
+    const span = document.createElement('span');
+    span.innerText = texto;
+    if(todoOk){
+        span.setAttribute('class', 'success');
+    } else {
+        span.setAttribute('class', 'error');
+    }
+    mensajeEl.appendChild(span);
+}
+// capturar los valores de los inputs
+// -Si la consulta sale bien mostrar un mensaje en verde 
+// -Si la consulta sale mal, mostrar un mensaje en rojo debajo del formulario
