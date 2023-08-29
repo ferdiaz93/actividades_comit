4185eaa77865a26f597f263823dc2af877eeebe5
Fernando Diaz <fernandodiaz1209@gmail.com>
2023-08-29 20:22:45 -0300

    comentarios agregado al index

--- clase_22/index.js
+++ clase_22/index.js
@@ -12,6 +12,7 @@ app.get('/users', async (req, res) => {
     console.log(process.env);
     console.log("Nueva peticion en users")
     await conectarDB();
+    // User.find es una promesa que retorna un array de usuarios
     const users = await User.find({});
     await desconectarDB();
     res.json(users);
@@ -20,7 +21,9 @@ app.get('/users', async (req, res) => {
 app.post('/user', async (req, res) => {
     const { name, email, age } = req.body;
     await conectarDB();
+    // creamos un usuario con new User y lo guardamos en la base de datos con save()
     const user = new User({ name, email, age });
+    // user.save() es una promesa que retorna el usuario guardado
     const newUser = await user.save();
     await desconectarDB();
     res.json(newUser);
@@ -29,3 +32,16 @@ app.post('/user', async (req, res) => {
 app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto " + PORT);
 });
+
+
+// instalar
+// npm i mongoose
+// npm i express
+// npm i dotenv
+// npm i nodemon -D
+
+// Actividad
+// Crear un enpoint GET /products
+// Crear un enpoint POST /product
+// Crear un Schema para Product
+// Crear un modelo para Product
